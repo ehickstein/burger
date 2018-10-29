c7ebb8dfd66218cba6912da96cf62d3b4425926c
ehickstein <38333223+ehickstein@users.noreply.github.com>
2018-10-29 18:57:38 -0500

    Final code

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom & Swiss Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
